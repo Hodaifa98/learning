33985c097abd2b5b58d3403943423b0f25627da4
hodaifa98 <houdaifa.alami@hotmail.com>
2024-05-20 14:43:27 +0100

    Create syntax.py

---
+++ Computer Science/Programming/Languages/Python/Basics/syntax.py
@@ -0,0 +1,10 @@
+""" Explaining python syntax """
+
+# Comments start with '#'.
+
+# A python module usually have a module docstring as the first line, 
+# listing what the module exports (classes, functions...).
+
+# Python uses indentation to indicate blocks of code.
+if 1 > 0:
+    print("Correct")
